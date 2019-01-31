29a9a34a4f048035043060b43d1f3bfd058cd566
savayer <sava0094@gmail.com>
2019-01-31 12:14:26 +0200

    added page create of tests

---
+++ src/renderer/components/TableTests.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="table_wrapper">
+    <table class="table">
+      <thead>
+        <th>№</th>
+        <th>Date</th>
+        <th>Name</th>
+        <th>Actions</th>
+      </thead>
+      <tbody>
+        <tr v-for="(test, index) in tests" :key="index">
+          <td>
+            {{ test.num }}
+          </td>
+          <td>
+            {{ test.date }}
+          </td>
+          <td>
+            {{ test.name }}
+          </td>
+          <td>
+            <button>edit</button>
+            <button>delete</button>
+            <button>Export</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  name: 'tableTests',
+  props: {
+    tests: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
---
+++ src/renderer/components/Test.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper">    
+    <router-link to="/new-test" class="link_button">
+      New Test
+    </router-link>    
+    <table-tests 
+      :tests="dataTests"
+    />
+  </div>
+</template>
+
+<script>
+  import tableTests from './TableTests'
+  export default {
+    name: 'test',
+    components: {
+      tableTests
+    },
+    data () {
+      return {
+        dataTests: [
+          { num: 1, date: (new Date()).toLocaleString(), name: '1st test' },
+          { num: 2, date: (new Date()).toLocaleString(), name: '2nd test' }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" src="./../assets/scss/styles.scss"></style>
