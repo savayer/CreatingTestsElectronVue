cadea5b31acb42c97fe6955989d64003b57f5e74
savayer <sava0094@gmail.com>
2019-01-31 12:15:33 +0200

    page of create new test

---
+++ src/renderer/components/NewTest.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="wrapper">
+        <router-link to="/" class="link_button">
+            Go back
+        </router-link>
+        <h1>Создание теста</h1>
+    </div>
+</template>
+<script>
+  export default {
+    name: 'new-test'
+  }
+</script>
